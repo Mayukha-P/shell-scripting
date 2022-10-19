7a40249f1dd221cd31eff5e76197b8c1fbda7f1f
mayukha19 <mayukhapullapantula@gmail.com>
2022-10-19 19:09:16 +0530

    msg

---
+++ color.sh
@@ -0,0 +1,23 @@
+#! bin/bash
+# practicing colors
+
+#Colors       Foreground          Background
+
+#Red               31                  41
+
+#Green             32                  42
+
+#Yellow            33                  43
+
+#Blue              34                  44
+
+#Magenta           35                  45
+
+#Cyan              36                  46
+
+echo -e "\e[33m Pineapple \e[0m"
+echo -e "\e[31m apple \e[0m"
+echo -e "\e[32m watermelon \e[0m"
+echo -e "\e[34m blue berry \e[0m"
+echo -e "\e[35m dragon fruit \e[0m"
+echo -e "\e[36m kiwi \e[0m"
