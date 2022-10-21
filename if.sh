d8b2c004ac29a191e2a680f60415947a5b904320
mayukha19 <mayukhapullapantula@gmail.com>
2022-10-21 19:58:21 +0530

    msg

--- if.sh
+++ if.sh
@@ -1,8 +1,10 @@
 #! bin/bash
 read -p "Enter a number: " NUM 
 echo "Your number is: $NUM"
-if  [ "$NUM" == "0" ]; then 
-    echo -e "\e[33m Value is null \e[0m"
+rem = ($NUM % 2)
+echo "Remainder is: $rem"
+if  [ "rem" == "0" ]; then 
+    echo -e "\e[33m Value is even \e[0m"
 else 
-     echo -e "\e[34m Value is valid \e[0m"
+     echo -e "\e[34m Value is odd \e[0m"
 fi 
