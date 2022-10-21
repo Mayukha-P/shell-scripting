cc3d4aa6a4a68d77b3f0fc09fea497a38e226f4a
mayukha19 <mayukhapullapantula@gmail.com>
2022-10-21 19:43:42 +0530

    msg

--- function.sh
+++ function.sh
@@ -12,11 +12,7 @@ stat()
 {
     read -p "Enter a number: " number
    echo -e "number is: $NUM"
-    if [ $NUM -eq 0]; then
-     echo -e "\e[31m number is null \e[0m"
-    else
-     echo -e "\e[33m number is not null \e[0m"
-    fi
+    
 
 
 
