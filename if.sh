be3edde9c43e21b450dffba7e70b38fe94ca1891
mayukha19 <mayukhapullapantula@gmail.com>
2022-10-21 19:52:51 +0530

    msg

--- if.sh
+++ if.sh
@@ -1,9 +1,8 @@
 #! bin/bash
 read -p "Enter a number" NUM 
-Your number is:$NUM
-
+Your number is: $NUM
 if  [ "$NUM" == "0" ]; then 
-    echo -e "\e[32m Value is null \e[0m"
+    echo -e "\e[33m Value is null \e[0m"
 else 
-     echo -e "\e[31m Value is valid \e[0m"
+     echo -e "\e[34m Value is valid \e[0m"
 fi 
