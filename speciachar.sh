3663593476fb0883943df2163c7920dd6c9d4351
mayukha19 <mayukhapullapantula@gmail.com>
2022-10-21 19:17:56 +0530

    msg

--- speciachar.sh
+++ speciachar.sh
@@ -1,8 +1,8 @@
 #! bin/bash
-echo $0
-echo $1
-echo $2
-echo $*
-echo $#
-echo $?
-echo $$
+echo "the script name you are running is:" $0
+echo " The first argument is" $1
+echo "The second argument is:" $2
+echo "The arguments are:" $*
+echo "The no.of args are:" $#
+echo "exit code of previous command is:" $?
+echo "the PID of process is:" $$
