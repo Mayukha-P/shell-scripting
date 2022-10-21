60fe1912aa1fcf66cc22a23cab39cc0927f50d4e
mayukha19 <mayukhapullapantula@gmail.com>
2022-10-21 19:34:33 +0530

    msg

--- function.sh
+++ function.sh
@@ -1,20 +1,28 @@
 #!bin/bash
-
+a=abc
 sample()
 {
-   read -p "Enter a number: " number
+   read -p "Enter a number: " NUM
+   echo -e "number is: $NUM"
    echo "calling stat"
    stat
 }
+echo "Calling sample"
 sample
 stat()
 {
    
-    if ["$number" == "0"]; then
-    echo -e "\e[31m number is null \e[0m"
+    if ["$NUM" == "0"]; then
+     echo -e "\e[31m number is null \e[0m"
     else
-    echo -e "\e[33m number is not null \e[0m"
+     echo -e "\e[33m number is not null \e[0m"
     fi
+
+if  [ "$a" == "abc" ]; then 
+    echo -e "\e[32m Both of the are equal \e[0m"
+else 
+     echo -e "\e[31m Both of the are not equal \e[0m"
+fi 
 
 
 }
