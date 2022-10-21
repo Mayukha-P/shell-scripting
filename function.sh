1946433c1fb3e9994031c7022678ee36b472c185
mayukha19 <mayukhapullapantula@gmail.com>
2022-10-21 19:36:15 +0530

    msg

--- function.sh
+++ function.sh
@@ -12,11 +12,7 @@ sample
 stat()
 {
    
-    if ["$NUM" == "0"]; then
-     echo -e "\e[31m number is null \e[0m"
-    else
-     echo -e "\e[33m number is not null \e[0m"
-    fi
+   
 
 if  [ "$a" == "abc" ]; then 
     echo -e "\e[32m Both of the are equal \e[0m"
