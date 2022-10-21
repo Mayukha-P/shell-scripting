c518bc68338a90cc6be59539e17ccecfc3cdaa34
mayukha19 <mayukhapullapantula@gmail.com>
2022-10-21 19:59:16 +0530

    msg

--- if.sh
+++ if.sh
@@ -1,7 +1,7 @@
 #! bin/bash
 read -p "Enter a number: " NUM 
 echo "Your number is: $NUM"
-rem = ($NUM % 2)
+rem = $(( $NUM % 10 ))
 echo "Remainder is: $rem"
 if  [ "rem" == "0" ]; then 
     echo -e "\e[33m Value is even \e[0m"
