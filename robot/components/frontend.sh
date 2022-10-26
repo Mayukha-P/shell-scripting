b6c2e29d7d5e4ccd2a982a4d9e270548c060553c
mayukha19 <mayukhapullapantula@gmail.com>
2022-10-26 22:46:39 +0530

    Cart

--- robot/components/frontend.sh
+++ robot/components/frontend.sh
@@ -1,11 +1,36 @@
 #!bin/bash
 
-echo -n "Installing yum: "
+set -e 
+
+COMPONENT=frontend
+
+source components/common.sh
+
+echo -n "Installing Ngnix:"
 yum install nginx -y &>> $LOGFILE
 stat $?
 
 echo -n "Downloading the component: "
 curl -s -L -o /tmp/frontend.zip "https://github.com/stans-robot-project/$COMPONENT/archive/main.zip"
 stat $?
-systemctl enable nginx
-systemctl start nginx 
+
+echo -n "Performing Cleanup:"
+rm -rf /usr/share/nginx/html/*  &>> $LOGFILE
+stat $?
+
+cd /usr/share/nginx/html
+echo -n "Unzipping the $COMPONENT:"
+unzip /tmp/$COMPONENT.zip &>> $LOGFILE
+stat $? 
+mv $COMPONENT-main/* .
+mv static/* .
+rm -rf $COMPONENT-main README.md &>> $LOGFILE
+
+echo -n "Conifuring the reverse proxy file:"
+mv localhost.conf /etc/nginx/default.d/roboshop.conf
+stat $?
+
+echo -n "Starting Frontend Service:"
+systemctl enable nginx &>> $LOGFILE
+systemctl start nginx &>> $LOGFILE
+stat $?
