09f7ccfaa5ea225858e9e03668b7aa0e98dcb6c0
mayukha19 <mayukhapullapantula@gmail.com>
2022-10-31 22:12:11 +0530

    ami id

--- robot/createserver.sh
+++ robot/createserver.sh
@@ -1,5 +1,5 @@
 #!bin/bash
-AMIID="$(aws ec2 describe-images --region us-east-1 --filters "Name=name,Values=DevOps-LabImage-CentOS7" | jq '.Images[].ImageId' | sed -e s/"//g)"
+AMIID="$(aws ec2 describe-images --region us-east-1 --filters "Name=name,Values=DevOps-LabImage-CentOS7" | jq '.Images[].ImageId' | sed -e 's/"//g')"
 
 echo $AMIID
 # ami-00ff427d936335825
