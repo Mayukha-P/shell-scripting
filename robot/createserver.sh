984cb3ef3b5357805005ba21ac20356df5f6a6fd
mayukha19 <mayukhapullapantula@gmail.com>
2022-10-31 22:46:37 +0530

    creating server

--- robot/createserver.sh
+++ robot/createserver.sh
@@ -1,4 +1,8 @@
 #!bin/bash
+if [ -z "$1" ]; then 
+    echo -e "\e[31m Component name is required \n example usage is: \n\t bash create-server.sh componentName \e[0m"   
+    exit 1 
+fi 
 COMPONENT=$1
 AMIID="$(aws ec2 describe-images --region us-east-1 --filters "Name=name,Values=DevOps-LabImage-CentOS7" | jq '.Images[].ImageId' | sed -e 's/"//g')"
 SGID="$(aws ec2 describe-security-groups   --filters Name=group-name,Values=b51-allow-all | jq '.SecurityGroups[].GroupId' | sed -e 's/"//g')"
