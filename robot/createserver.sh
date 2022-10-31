0f5bdbf977b411e5641955e2f6526e717bf80312
mayukha19 <mayukhapullapantula@gmail.com>
2022-10-31 22:25:40 +0530

    ami id

--- robot/createserver.sh
+++ robot/createserver.sh
@@ -4,4 +4,4 @@ AMIID="$(aws ec2 describe-images --region us-east-1 --filters "Name=name,Values=
 echo $AMIID
 # ami-00ff427d936335825
 # DevOps-LabImage-CentOS7
-aws ec2 run-instances --image-id $AMIID --instance-type t2.micro
+aws ec2 run-instances --image-id $AMIID --instance-type t2.micro | jq 
