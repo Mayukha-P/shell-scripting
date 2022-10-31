6aa3cb624ab252cc146dc001ec3ee663fd0366c5
mayukha19 <mayukhapullapantula@gmail.com>
2022-10-31 22:44:17 +0530

    creating server

--- robot/createserver.sh
+++ robot/createserver.sh
@@ -2,8 +2,7 @@
 COMPONENT=$1
 AMIID="$(aws ec2 describe-images --region us-east-1 --filters "Name=name,Values=DevOps-LabImage-CentOS7" | jq '.Images[].ImageId' | sed -e 's/"//g')"
 SGID="$(aws ec2 describe-security-groups   --filters Name=group-name,Values=b51-allow-all | jq '.SecurityGroups[].GroupId' | sed -e 's/"//g')"
-echo $AMIID
-aws ec2 run-instances --image-id $AMIID --instance-type t2.micro --security-group-ids $SGID --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${COMPONENT}}]"| jq 
-echo "AMI ID Used to launch instance is : $AMI_ID"
+echo "AMI ID Used to launch instance is : $AMIID"
 echo "SG ID Used to launch instance is : $SGID"
 echo $COMPONENT
+aws ec2 run-instances --image-id $AMIID --instance-type t2.micro --security-group-ids $SGID --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${COMPONENT}}]"| jq 
