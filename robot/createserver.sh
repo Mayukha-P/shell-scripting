cef7759d9c0c98374258cc8172329ac8315ded8e
mayukha19 <mayukhapullapantula@gmail.com>
2022-11-01 18:21:31 +0530

    creating DNS records

--- robot/createserver.sh
+++ robot/createserver.sh
@@ -9,4 +9,13 @@ SGID="$(aws ec2 describe-security-groups   --filters Name=group-name,Values=b51-
 echo "AMI ID Used to launch instance is : $AMIID"
 echo "SG ID Used to launch instance is : $SGID"
 echo $COMPONENT
-aws ec2 run-instances --image-id $AMIID --instance-type t2.micro --security-group-ids $SGID  --instance-market-options "MarketType=spot, SpotOptions={SpotInstanceType=persistent,InstanceInterruptionBehavior=stop}" --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${COMPONENT}}]"| jq 
+#creating server
+createserver()
+{
+PRIVATE_IP=$(aws ec2 run-instances --image-id $AMIID --instance-type t2.micro --security-group-ids $SGID  --instance-market-options "MarketType=spot, SpotOptions={SpotInstanceType=persistent,InstanceInterruptionBehavior=stop}" --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${COMPONENT}}]"| jq '.Instances[].PrivateIpAddress' | sed -e 's/"//g')
+sed -e "s/IPADDRESS/${PRIVATE_IP}/" -e "s/COMPONENT/$COMPONENT/" route53.json > /tmp/dns.json 
+
+    echo -n "\e[35m ****Creating the DNS Record *****\e[0m"
+    aws route53 change-resource-record-sets --hosted-zone-id $ZONE_ID --change-batch file:///tmp/dns.json | jq 
+} 
+
