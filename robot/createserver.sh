f9a6ce3b53353a4aea69a9e3d2e496df8612ae01
mayukha19 <mayukhapullapantula@gmail.com>
2022-11-01 18:50:16 +0530

    creating all servers

--- robot/createserver.sh
+++ robot/createserver.sh
@@ -16,7 +16,7 @@ createserver()
 PRIVATE_IP=$(aws ec2 run-instances --image-id $AMIID --instance-type t2.micro --security-group-ids $SGID  --instance-market-options "MarketType=spot, SpotOptions={SpotInstanceType=persistent,InstanceInterruptionBehavior=stop}" --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${COMPONENT}}]"| jq '.Instances[].PrivateIpAddress' | sed -e 's/"//g')
 sed -e "s/IPADDRESS/${PRIVATE_IP}/" -e "s/COMPONENT/$COMPONENT/" route53.json > /tmp/dns.json 
 
-    echo -n "\e[35m ****Creating the DNS Record *****\e[0m"
+    echo -e "\e[35m ****Creating the DNS Record ***** \e[0m"
     aws route53 change-resource-record-sets --hosted-zone-id $ZONE_ID --change-batch file:///tmp/dns.json | jq 
 } 
 
